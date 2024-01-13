ca7680878eddb343658d6d8c6c61344036173c9c
Somhairle MacLeòid <samuel@macleod.space>
2024-01-13 23:51:59 +0000

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: "server",
+  site: 'https://www.coisirlunnainn.org',
   adapter: cloudflare()
 });
