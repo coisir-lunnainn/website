acbb89fe4fa52da4d4999a8b53f1d4f26c50ca13
Somhairle MacLeòid <samuel@macleod.space>
2024-01-14 19:16:49 +0000

    typing

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+type ENV = {
+  GH_PAT: string;
+};
+
+type Runtime = import("@astrojs/cloudflare").AdvancedRuntime<ENV>;
+
+declare namespace App {
+  interface Locals extends Runtime {}
+}
