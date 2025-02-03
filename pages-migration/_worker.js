ae2c565810c2a468d5e1e2ffa7d824f21ce29e06
Somhairle MacLeòid <samuel@macleod.space>
2025-02-03 12:30:18 +0000

    Update _worker.js

--- pages-migration/_worker.js
+++ pages-migration/_worker.js
@@ -1,7 +1,7 @@
 export default {
   fetch(request) {
     const url = new URL(request.url);
-    url.hostname = "coisirlunnainn.dev";
+    url.hostname = "london-gaelic-choir.c-isir-lunnainn.workers.dev";
     return fetch(url.toString(), request);
   }
 }
